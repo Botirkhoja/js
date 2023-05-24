9578af45038586ce5c5529ffe4b832a48ff094d8
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-24 18:51:08 +0300

    Add files via upload

---
+++ goblis/app.js
@@ -0,0 +1,98 @@
+// let var const 
+
+// es5
+// var
+
+
+
+// es6 
+// let const 
+
+// let yosh = 30;
+
+// yosh = 35;
+
+// let age = 27;
+// console.log(age);
+
+
+
+
+
+
+// age = 30;
+
+// console.log(age);
+
+// string = "uguyftfgd";
+
+// NUmber = 8784567890;
+
+// BIgINt = 1990n;
+
+// Boolean = true / false 
+
+// undifined= bor lekin yoq 
+
+// null = xato/ 0 ga teng / yoq;
+
+// Symbol = @#$%^&*;
+// Object = {} [];
+
+
+
+
+
+// if (studentBaho == 5) {
+//     alert("Barakalla bolam 5 boho")
+// } else {
+//     alert("Yaxshiroq o'qi bolajon")
+// }
+
+
+// let studentBaho = 2;
+
+
+// if (studentBaho >= 5) {
+//     console.log("Barakalla");
+// } else if (studentBaho == 4) {
+//     console.log("Normal");
+// } else if (studentBaho == 3 ) {
+//     console.log("Yaxshiroq oqi bomasam kalte yisan ");
+// }else if (studentBaho == 2) {
+//     console.log("Kalte uyda sani kutyapti");
+// }else{
+//     console.log("Deddomda yashesan endi");
+// }
+
+
+
+// let html = false;
+// let css = false;
+
+// if (html && css) {
+//     console.log("EEE Barakalla bolarkanu");
+// }else if (html || css){
+//     console.log("Narigi kursdiyam oqin brat");
+// }else {
+//     console.log("Bowqattan oqin bratwka");
+// }
+
+
+
+
+
+
+
+
+
+
+let yosh = "Salom"; // string
+console.log(yosh); // string
+
+console.log(Number(yosh))
+
+
+console.log(yosh + 1)
+
+console.log(Number(yosh) + 1)
